ef7587ad607dc423abff937df736c34d3e27524a
IUTB <noreply@univ-tlse2.fr>
2022-05-11 10:29:15 +0200

    Put new workspace

--- application/BLACKJACK/BlackJack V2/src/application/model/EtatBlackBot.java
+++
@@ -1,7 +0,0 @@
-package application.model;
-
-public enum EtatBlackBot {
-    MISE, // Init d'une partie : mises en cours
-	JEU,  // Jeu en cours      : chaque joueur tire ou Passe
-	GAIN  // Fin d'une partie  : Le croupier a tiré, les gains sont calculés
-}
