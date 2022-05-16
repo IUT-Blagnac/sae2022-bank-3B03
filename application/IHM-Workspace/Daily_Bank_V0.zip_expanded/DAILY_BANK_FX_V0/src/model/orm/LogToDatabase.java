d367c6e37e4f6849f2c9bb52b5cea768822b9076
IUTB <noreply@univ-tlse2.fr>
2022-05-16 10:51:16 +0200

    change database passwd

--- application/IHM-Workspace/Daily_Bank_V0.zip_expanded/DAILY_BANK_FX_V0/src/model/orm/LogToDatabase.java
+++ application/IHM-Workspace/Daily_Bank_V0.zip_expanded/DAILY_BANK_FX_V0/src/model/orm/LogToDatabase.java
@@ -16,8 +16,8 @@ import model.orm.exception.DatabaseConnexionException;
 public class LogToDatabase {
 
 	// Gloabl : accès à la base de donnée : à ajuster selon le compte
-	private static final String user = "";
-	private static final String passwd = "";
+	private static final String user = "G3B3";
+	private static final String passwd = "pAy4L0gIN";
 
 	private static final String hoteOracle = "oracle.iut-blagnac.fr";
 	private static final String portOracle = "1521";
