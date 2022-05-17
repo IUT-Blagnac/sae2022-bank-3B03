eef6e42a24f3be1f2ddc97bd58ea32428037f240
IUTB <hugo.castell@outlook.fr>
2022-05-17 09:11:58 +0200

    CREDITER : mise à jour BACK pour access procedure stockée

--- application/IHM-Workspace/Daily_Bank_V0.zip_expanded/DAILY_BANK_FX_V0/src/model/orm/AccessOperation.java
+++ application/IHM-Workspace/Daily_Bank_V0.zip_expanded/DAILY_BANK_FX_V0/src/model/orm/AccessOperation.java
@@ -156,6 +156,52 @@ public class AccessOperation {
 			throw new DataAccessException(Table.Operation, Order.INSERT, "Erreur accès", e);
 		}
 	}
+	
+	/**
+	 * 
+	 * Enregistrement d'un crédit.
+	 *
+	 * Se fait par procédure stockée : - Enregistre l'opération - Met à jour le solde du compte.
+	 * 
+	 * @author Hugo CASTELL
+	 * 
+	 * @param idNumCompte
+	 * @param montant
+	 * @param typeOp
+	 * @throws DatabaseConnexionException
+	 * @throws ManagementRuleViolation
+	 * @throws DataAccessException
+	 */
+	public void insertCredit(int idNumCompte, double montant, String typeOp)
+			throws DatabaseConnexionException, ManagementRuleViolation, DataAccessException {
+		try {
+			Connection con = LogToDatabase.getConnexion();
+			CallableStatement call;
+
+			String q = "{call CREDITER (?, ?, ?, ?)}";
+			// les ? correspondent aux paramètres : cf. déf procédure (4 paramètres)
+			call = con.prepareCall(q);
+			// Paramètres in
+			call.setInt(1, idNumCompte);
+			// 1 -> valeur du premier paramètre, cf. déf procédure
+			call.setDouble(2, montant);
+			call.setString(3, typeOp);
+			// Paramètres out
+			call.registerOutParameter(4, java.sql.Types.INTEGER);
+			// 4 type du quatrième paramètre qui est déclaré en OUT, cf. déf procédure
+
+			call.execute();
+
+			int res = call.getInt(4);
+
+			if (res != 0) { // Erreur applicative
+				throw new ManagementRuleViolation(Table.Operation, Order.INSERT,
+						"Erreur de règle de gestion : découvert autorisé dépassé", null);
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException(Table.Operation, Order.INSERT, "Erreur accès", e);
+		}
+	}
 
 	/**
 	 * Fonction utilitaire qui retourne un ordre sql "to_date" pour mettre une date
